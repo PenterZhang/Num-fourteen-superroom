76d616add3373e309adc7d3ca3603fb59c2ebd45
Penter Zhang <z1992752625@outlook.com>
2017-11-01 19:18:59 +0800

    11.1项目上传

--- 良仓-张朋涛/JS/Lcang.js
+++ 良仓-张朋涛/JS/Lcang.js
@@ -1,11 +1,5 @@
-/*
-$指一个函数，回调函数
-搜索框思路：
-1.获取输入框和放大镜，
-2.当输入框的right值为-192的时候点击让它右移210px;
-3.当right值为210的时候点击让它右移210px;
-*/
 
+//搜索框显示与隐藏
 $("#magnifier").click(function(){
     if($("#sch").css("marginLeft")=="226px"){
         $("#sch").animate({"marginLeft":0});
@@ -13,12 +7,110 @@ $("#magnifier").click(function(){
         $("#sch").animate({"marginLeft":226});
     }
 });
-/*
-$("#magnifier").click(function(){ 
-    if($("#sch").css("right")=="-192px"){
-        $("#sch").animate({"right":210});
-    }else{
-        $("#sch").animate({"right":-210});
-    }
+
+
+//轮播图
+
+
+var w = $("#m_unit ul li").innerWidth();
+$("#rightBtn").click(function() {
+	move();
 });
-  */
+setInterval(move, 1000);
+var i = 0;
+function move() {
+	if ($("#m_unit").is(":animated")) {
+		return ; 
+	}
+	i++;
+	$("#m_unit").animate({"left": -i*w}, 1000, function() {
+		if (i >= 5) {
+			i = 0; 
+			$("#m_unit").css({"left": 0});
+		}
+		$(".circles ol li").removeClass("current");
+		$(".circles ol li").eq(i).addClass("current");
+	});
+}
+
+
+
+
+
+
+
+//获取商品参数
+var url = "http://h6.duchengjiu.top/shop/api_goods.php";
+var data = {
+    "cat_id":"0",
+    // "page":"0",
+    // "pagesize":"0"
+}
+var a=[];
+$.ajax({
+    url:url,
+    type:"GET",
+    data:data,
+    success:function(str){
+        var a=str.data;
+        console.log(str);
+        for(var i=0;i<a.length-1;i++){    
+        $(".shoppings ul").append("<li></li>");
+        $(".shoppings ul li").eq(i).css("background-image","url("+a[i].goods_thumb+")");
+        if(i%3==0){
+            $(".shoppings ul li").eq(i).css("margin-left","0");
+        }
+        }
+    $(".shoppings ul li").each(function(i){
+            $(this).click(function(){
+                window.open("lc-xiangqing.html?"+a[i].goods_id);
+            })
+        }) 
+        $(".shoppings ul li").append("<div></div>");
+        $(".shoppings ul li").each(function(i){
+            $(this).children("div").append("<i>"+"￥"+a[i].price+"</i>");
+        })
+        $(".shoppings ul li").each(function(i){
+            $(this).children("div").append("<h3>"+a[i].goods_name+"</h3>");
+        })
+        $(".shoppings ul li").each(function(i){
+            $(this).children("div").append("<p>"+a[i].goods_desc+"</p>");
+        })
+    }
+})
+window.onload = function (){
+    
+}
+
+
+
+
+
+
+//良仓仓库小手动画
+
+$(".service a").mouseover(function(){
+    $(".hand").animate({"marginLeft":-85});
+    $(".hand").animate({"marginLeft":-60});
+    $(".hand").animate({"marginLeft":-85});
+    $(".hand").animate({"marginLeft":-55});
+})
+
+
+
+
+
+
+
+// 点击返回顶部
+$(".call").click(function() {
+	$("body,html").animate({scrollTop: 0});
+});
+$(document).scroll(function() {
+	var top = $(document).scrollTop();
+	if (top > 200) {
+		$(".call").show(1000);
+	} else {
+		$(".call").hide(1000);
+	}
+})
